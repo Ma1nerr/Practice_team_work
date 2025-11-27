6310140a9a628bf2e8f16ca2f6779506f43c01ad
Nazarii Fito <nazariifito@MacBook-Pro-Nazarii.local>
2025-11-27 22:56:55 +0200

    Added another features to test merge conflicts

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,15 @@ import React from "react";
 const Header = () => {
   return (
     <header style={styles.header}>
-      <h1>My dream team</h1>
+      <h1>Our team 5</h1>
     </header>
   );
 };
 
 const styles = {
   header: {
-    padding: "1em",
-    color: "#6a0dad",
+    padding: "3em",
+    color: "#00ff91ff",
     textAlign: "center",
     fontWeight: "bold",
   },
